6c9b0d07c6c81c9482e21ecda8642e324a0c269b
DavidIsrael <davidisrael.rj@gmail.com>
2024-05-11 02:02:23 -0300

    Estrutura do projeto

---
+++ src/views/App.jsx
@@ -0,0 +1,12 @@
+import Menu from '../components/layout/Menu';
+import './App.css';
+import React from "react";
+
+const App = props =>(
+    <div className="App">
+        Componente App
+        <Menu/>
+    </div>
+)
+
+export default App
---
+++ src/views/examples/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = props =>(
+    <div>
+        <h1>Início</h1>
+        <p>Bem Vindo!!</p>
+    </div>
+)
+
+export default Home
